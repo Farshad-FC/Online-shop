20608cdca5d890f708b5787c5d98bc81600cdc79
Farshad-FC <fastcode.co@gmail.com>
2022-05-17 23:58:39 +0430

    add method getByUsernameAndPassword in UserRepository

---
+++ src/com/maktab74/OnlineShop/repository/UserRepository.java
@@ -0,0 +1,44 @@
+package com.maktab74.OnlineShop.repository;
+
+import com.maktab74.OnlineShop.domain.Address;
+import com.maktab74.OnlineShop.domain.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserRepository {
+    private Connection connection;
+
+    public UserRepository(Connection connection) {
+        this.connection = connection;
+    }
+
+    public User getByUsernameAndPassword(String username, String password) throws SQLException {
+        String query = "select * from user where username = ? and password = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, username);
+        preparedStatement.setString(2, password);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()) {
+            return new User(
+                    resultSet.getInt(1),
+                    resultSet.getString(2),
+                    resultSet.getString(3),
+                    resultSet.getString(4),
+                    resultSet.getString(5),
+                    resultSet.getString(6),
+                    resultSet.getString(7),
+                    new Address(
+                            resultSet.getString(8),
+                            resultSet.getString(9),
+                            resultSet.getString(10),
+                            resultSet.getString(11)
+                    )
+            );
+        }
+
+        return null;
+    }
+}
