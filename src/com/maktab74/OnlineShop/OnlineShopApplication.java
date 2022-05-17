3fc4346aea0184c6316d350252a9d9bb96b519bd
Farshad-FC <fastcode.co@gmail.com>
2022-05-17 23:54:00 +0430

    add main menu

--- src/com/maktab74/OnlineShop/OnlineShopApplication.java
+++ src/com/maktab74/OnlineShop/OnlineShopApplication.java
@@ -1,7 +1,45 @@
 package com.maktab74.OnlineShop;
 
-public class OnlineShopApplication {
-    public static void main(String[] args) {
+import com.maktab74.OnlineShop.util.ApplicationContext;
 
+import java.sql.SQLException;
+import java.text.ParseException;
+
+public class OnlineShopApplication {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        ApplicationContext context = new ApplicationContext();
+
+        context.getDatabaseInitializer().init();
+
+        boolean globalLoop = true;
+
+        while (globalLoop) {
+            context.getMenu().showWelcomeMenu();
+            boolean isSelectedNumberInValid = true;
+            while (isSelectedNumberInValid) {
+                int selectedNumber = context.getNumberScanner().nextInt();
+                if (selectedNumber == 1 || selectedNumber == 2) {
+                    isSelectedNumberInValid = false;
+                    redirectUser(selectedNumber, context);
+                } else if (selectedNumber == 3) {
+                    isSelectedNumberInValid = false;
+                    context.getMenu().showExitMessage();
+                    globalLoop = false;
+                } else {
+                    context.getMenu().showIncorrectNumberMessage();
+                }
+            }
+        }
+    }
+
+    private static void redirectUser(int selectedNumber, ApplicationContext context) throws SQLException, ParseException {
+        if (selectedNumber == 1) {
+            boolean isLoginSuccess = login(context);
+            if (isLoginSuccess) {
+                redirectUserToPanel(context);
+            }
+        } else {
+            signup(context);
+        }
     }
 }
