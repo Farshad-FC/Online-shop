80287d52b2f22de894868b73189477a4f7c8cae6
Farshad-FC <fastcode.co@gmail.com>
2022-05-28 15:03:53 +0430

    add method getCountShoe in ShoeRepository

--- src/com/maktab74/OnlineShop/repository/ShoeRepository.java
+++ src/com/maktab74/OnlineShop/repository/ShoeRepository.java
@@ -25,10 +25,25 @@ public class ShoeRepository {
         while (resultSet.next()) {
             shoe = new Shoe
                     (
-
+                            resultSet.getInt(1),
+                            resultSet.getString(5),
+                            resultSet.getInt(6),
+                            resultSet.getInt(7),
+                            resultSet.getString(2),
+                            resultSet.getString(3),
+                            resultSet.getString(4)
                     );
             shoes.add(shoe);
         }
         return shoes;
+    }
+
+    public int getCountShoe() throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select count(id) from shoe");
+        if (resultSet.next()) {
+            return resultSet.getInt(1);
+        }
+        return 0;
     }
 }
