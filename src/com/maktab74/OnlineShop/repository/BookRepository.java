9ea8b8569f65f519ead051bf8b99dfbde14f1ef6
Farshad-FC <fastcode.co@gmail.com>
2022-05-28 14:56:12 +0430

    add method getCountBook in BookRepository

--- src/com/maktab74/OnlineShop/repository/BookRepository.java
+++ src/com/maktab74/OnlineShop/repository/BookRepository.java
@@ -24,10 +24,28 @@ public class BookRepository {
         while (resultSet.next()) {
             book = new Book
                     (
-
+                            resultSet.getInt(1),
+                            resultSet.getString(8),
+                            resultSet.getInt(9),
+                            resultSet.getInt(10),
+                            resultSet.getString(2),
+                            resultSet.getString(4),
+                            resultSet.getString(5),
+                            resultSet.getString(6),
+                            resultSet.getString(7),
+                            resultSet.getString(3)
                     );
             books.add(book);
         }
         return books;
+    }
+
+    public int getCountBook() throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select count(id) from book");
+        if (resultSet.next()) {
+            return resultSet.getInt(1);
+        }
+        return 0;
     }
 }
