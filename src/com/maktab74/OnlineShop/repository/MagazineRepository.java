4fcc2a823e0a03296b73bdc3ca98df2b59522c26
Farshad-FC <fastcode.co@gmail.com>
2022-05-28 14:59:41 +0430

    add method getCountMagazine in MagazineRepository

--- src/com/maktab74/OnlineShop/repository/MagazineRepository.java
+++ src/com/maktab74/OnlineShop/repository/MagazineRepository.java
@@ -24,10 +24,28 @@ public class MagazineRepository {
         while (resultSet.next()) {
             magazine = new Magazine
                     (
-
+                            resultSet.getInt(1),
+                            resultSet.getString(8),
+                            resultSet.getInt(9),
+                            resultSet.getInt(10),
+                            resultSet.getString(2),
+                            resultSet.getString(3),
+                            resultSet.getString(4),
+                            resultSet.getString(5),
+                            resultSet.getString(7),
+                            resultSet.getString(6)
                     );
             magazines.add(magazine);
         }
         return magazines;
+    }
+
+    public int getCountMagazine() throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select count(id) from magazine");
+        if (resultSet.next()) {
+            return resultSet.getInt(1);
+        }
+        return 0;
     }
 }
